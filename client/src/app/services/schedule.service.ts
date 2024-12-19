e3c8d90cb45a3d2c666ffb94dfc77e7b5876a95c
ArgasDTiger <bodia02981@gmail.com>
2024-12-19 22:26:40 +0200

    forgot to add something

---
+++ client/src/app/services/schedule.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  constructor() { }
+
+  getByGroup(groupNumber: number) {
+    return
+  }
+}
